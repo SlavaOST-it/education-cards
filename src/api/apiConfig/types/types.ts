db6360793293d0e24f59a8024befc4b8345212a0
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-10 21:41:44 +0300

    feat: add types for Profile, Auth

--- src/api/apiConfig/types/types.ts
+++ src/api/apiConfig/types/types.ts
@@ -1,3 +1,5 @@
+
+// ===== AUTH TYPE ===== //
 export type RegisterResponseType = {
     addedUser: {}
     error?: string;
@@ -10,3 +12,57 @@ export type LoginResponseType = {
     rememberMe: boolean,
     publicCardPacksCount: number
 }
+
+
+// ===== PROFILE TYPE ===== //
+export type UserType = {
+    _id: string;
+    email: string;
+    name: string;
+    avatar: null | string;
+    publicCardPacksCount: number;
+    rememberMe: boolean;
+}
+
+
+// ===== PACKS TYPE ===== //
+export type PacksParamsType = {
+    packName?: string;
+    min?: string;
+    max?: string;
+    sortPacks?: string;
+    page?: string;
+    pageCount?: string;
+    user_id?: string;
+};
+
+export type PacksType = {
+    cardPacks: PackType[];
+    page: number;
+    user_id: string;
+    pageCount: number;
+    cardPacksTotalCount: number;
+    minCardsCount: number;
+    maxCardsCount: number;
+    token: string;
+};
+
+export type PackType = {
+    _id: string;
+    cardsCount: number;
+    created: string;
+    grade: number;
+    more_id: string;
+    path: string;
+    name: string;
+    type: string;
+    private: boolean;
+    rating: number;
+    shots: number;
+    updated: string;
+    user_id: string;
+    user_name: string;
+    deckCover: string;
+};
+
+// ===== CARDS TYPE ===== //
