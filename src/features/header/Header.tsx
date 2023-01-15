4b4835261fbc66a4faa1846ce095f55e9e2ea568
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-15 19:54:48 +0300

    fix: add loading line

--- src/features/header/Header.tsx
+++ src/features/header/Header.tsx
@@ -4,14 +4,19 @@ import incubatorLogo from "../../assets/img/icons/incubator_logo.png"
 import s from './Header.module.css'
 import {HeaderItem} from "./headerItem/HeaderItem";
 import {SignInButton} from "../../common/components/buttons/signInButton/SignInButton";
+import {AppStatus} from "../../common/types/types";
+import LinearProgress from "@mui/material/LinearProgress";
 
 
 export const Header = () => {
     const loggedIn = useAppSelector(state => state.login.loggedIn)
+    const appStatus = useAppSelector((state) => state.app.status)
 
     return (
         <div className={s.header}>
+
             <div className={s.headerWrapper}>
+
                 <div>
                     <a href="https://it-incubator.io/" target="_blank" className={s.logo} rel="noreferrer">
                         <img src={incubatorLogo} alt="incubatorLogo"/>
@@ -23,6 +28,7 @@ export const Header = () => {
                     : <SignInButton/>
                 }
             </div>
+                {appStatus === AppStatus.LOADING && <LinearProgress/>}
         </div>
     )
 };
