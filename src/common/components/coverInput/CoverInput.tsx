3dbe8d13a57e75230dbb2c2f7014a22639b0a701
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-17 23:11:00 +0300

    fix: refactoring, fix bug, add styles

--- src/common/components/coverInput/CoverInput.tsx
+++ src/common/components/coverInput/CoverInput.tsx
@@ -1,64 +1,82 @@
 import React, {ChangeEvent, FC, useEffect, useState} from 'react';
 import style from "../modals/addPackModal/AddPackModal.module.css";
-import Button from "@mui/material/Button";
 import {setDeckCoverAC} from "../../../bll/reducers/packs-reducer";
 import {useAppDispatch, useAppSelector} from "../../../utils/hooks/hooks";
+import cameraLogo from "../../../assets/img/icons/camera-svgrepo-com.svg"
 import {baseDeckCover} from "../../../assets/baseDeckCover";
+import {convertFileToBase64} from "../../../utils/convertFileToBase64/convertFileToBase64";
+import {setAppErrorAC} from "../../../bll/reducers/app-reducer";
 
-type CoverInputType={
-    deckCover:string
+
+type CoverInputType = {
+    deckCover: string
 }
 
-export const CoverInput:FC<CoverInputType> = ({deckCover}) => {
-    const dispatch =useAppDispatch()
-    const myDeckCover=useAppSelector(state=>state.packs.coverImg)
+export const CoverInput: FC<CoverInputType> = ({deckCover}) => {
+    const dispatch = useAppDispatch()
+    const myDeckCover = useAppSelector(state => state.packs.coverImg)
+
     const [isCoverBroken, setIsCoverBroken] = useState(false)
 
     const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files.length) {
-            const file = e.target.files[0]
+        const value = e.target.files
+        if (value && value.length) {
+            const file = value[0]
 
             if (file.size < 100000) {
-                const reader = new FileReader();
-                reader.onloadend = () => {
-                    const file64 = reader.result as string
-                    dispatch(setDeckCoverAC( file64))
-                }
-                reader.readAsDataURL(file)
+                convertFileToBase64(file, (file64: string) => {
+                    dispatch(setDeckCoverAC(file64))
+                })
             } else {
-                alert('Файл слишком большого размера')
+                dispatch(setAppErrorAC('Файл слишком большого размера'))
             }
         }
     }
+
     const errorHandler = () => {
         setIsCoverBroken(true)
-        alert('Кривая картинка')
+        dispatch(setAppErrorAC('Broken picture'))
     }
 
-    useEffect(()=>{
-        if(deckCover){
+    if (myDeckCover === "") {
+        dispatch(setDeckCoverAC(baseDeckCover))
+    }
+
+    useEffect(() => {
+        if (deckCover) {
             dispatch(setDeckCoverAC(deckCover))
         }
-    },[])
+    }, [])
+
+    const deleteCoverHandler = () => {
+        dispatch(setDeckCoverAC(''))
+    }
 
     return (
-        <div className={style.coverBlock}>
-            <div className={style.coverHeader}>
-                <div>Cover</div>
-                <div> <label>
+        <div className={style.wrapper}>
+
+            <p>Add an image to the Pack (optional)</p>
+
+            <div className={style.coverBlock}>
+                <label className={style.uploadBlock}>
                     <input type="file"
                            onChange={uploadHandler}
                            style={{display: 'none'}}
                     />
-                    <Button variant="contained" component="span">
-                        Upload button
-                    </Button>
-                </label> </div>
-            </div>
-            <div className={style.coverImage}>
-                <img onError={errorHandler} src={ isCoverBroken ? baseDeckCover : myDeckCover} alt="cover"/>
+                    <img src={cameraLogo} alt={'upload cover'} className={style.cameraLogo}/>
+                </label>
+
+                <div className={style.coverImage}>
+                    <img onError={errorHandler} src={isCoverBroken ? baseDeckCover : myDeckCover} alt="cover"/>
+                </div>
+
+                {myDeckCover !== baseDeckCover
+                    ? (<button className={style.deleteCoverBtn} onClick={deleteCoverHandler}>
+                        X
+                    </button>)
+                    : (<div className={style.deleteCoverDiv}></div>)
+                }
             </div>
         </div>
     );
 };
-
