07c8d8f7f27066d42a248fa02fbab9bd52c49702
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-19 21:44:07 +0300

    fix: fix

--- src/features/cards/ratingCards/RatingCard.tsx
+++ src/features/cards/ratingCards/RatingCard.tsx
@@ -1,29 +1,18 @@
 import React, {FC} from 'react';
-import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
-
 
 
 type BasicRatingType = {
     grade: number
 }
 
-export const BasicRating: FC<BasicRatingType> = ({grade})=> {
-    const [value, setValue] = React.useState<number | null>(grade);
+export const BasicRating: FC<BasicRatingType> = ({grade}) => {
 
     return (
-        <Box
-            sx={{
-                '& > legend': { mt: 2 },
-            }}
-        >
-            <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                    setValue(newValue);
-                }}
-            />
-        </Box>
+        <Rating
+            name="simple-controlled"
+            value={grade}
+        />
+
     );
 }
