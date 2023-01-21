04e97ed9d6b2af396ffaab0ae8bc2ae428edfd86
Vyacheslav Ostapkevich <slavaost-it@mail.ru>
2023-01-22 01:56:13 +0300

    fix: add selector for add text or image

---
+++ src/common/components/modals/cardsModals/addCardModal/AddCardModal.tsx
@@ -0,0 +1,116 @@
+import React, {ChangeEvent, FC, useState} from 'react';
+import s from "./AddCardModal.module.css"
+import {BasicModal} from "../../BasicModal";
+import TextField from "@mui/material/TextField";
+import {useAppDispatch} from "../../../../../utils/hooks/hooks";
+import {addCardTC} from "../../../../../bll/reducers/cards-reducer";
+import {FormControl, FormGroup, InputLabel, MenuItem, Select} from "@mui/material";
+import {SelectChangeEvent} from "@mui/material/Select";
+import {ImageQuestionLoader} from "../imageQuestionLoader/ImageQuestionLoader";
+
+
+const styleButtonMUI = {
+    borderRadius: 10,
+    width: 120
+}
+type AddCardsModalType = {
+    packId: string
+    active: boolean
+    setActive: (active: boolean) => void
+}
+export const AddCardModal: FC<AddCardsModalType> = ({
+                                                                packId,
+                                                                active,
+                                                                setActive
+                                                            }) => {
+    const dispatch = useAppDispatch()
+
+    const [typeQuestion, setTypeQuestion] = useState<"text" | "image" | "">("")
+    const [valueQuestion, setValueQuestion] = useState("")
+    const [imageQuestion, setImageQuestion] = useState("")
+    const [valueAnswer, setValueAnswer] = useState("")
+
+    const onChangeQuestionHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setValueQuestion(e.currentTarget.value)
+    }
+
+    const onChangeAnswerHandler = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        setValueAnswer(e.currentTarget.value)
+    }
+
+    const changeTypeQuestionHandler = (event: SelectChangeEvent) => {
+        setTypeQuestion(event.target.value as "text" | "image");
+    };
+
+    const onSaveHandler = () => {
+        dispatch(addCardTC(packId, valueQuestion, valueAnswer, imageQuestion))
+        setTypeQuestion("")
+        setValueQuestion("")
+        setImageQuestion("")
+        setValueAnswer("")
+        setActive(false)
+    }
+
+    const onCancelHandler = () => {
+        setTypeQuestion("")
+        setValueQuestion("")
+        setValueAnswer("")
+        setActive(false)
+    }
+
+    const disabledButton = typeQuestion === "image" ? imageQuestion === "" : valueQuestion.length === 0
+
+    return (
+        <BasicModal
+            title={"Add new card"}
+            nameButton={"Save"}
+            active={active}
+            setActive={onCancelHandler}
+            onSaveCallback={onSaveHandler}
+            disabledButton={disabledButton || valueAnswer.length === 0}
+            styleButton={styleButtonMUI}
+        >
+            <div className={s.questionBlock}>
+                <h4>Choose question format</h4>
+                <FormControl fullWidth>
+                    <FormGroup>
+                        <InputLabel htmlFor="question-format">format</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="question-format"
+                            value={typeQuestion}
+                            label="Question"
+                            onChange={changeTypeQuestionHandler}
+                        >
+                            <MenuItem value={"text"}>Text question</MenuItem>
+                            <MenuItem value={"image"}>Image question</MenuItem>
+                        </Select>
+                    </FormGroup>
+                </FormControl>
+
+                <div className={s.valueBlock}>
+                    {typeQuestion === "text" &&
+                        <TextField value={valueQuestion} label="Enter your question" margin="normal" fullWidth={true}
+                                   onChange={onChangeQuestionHandler}/>
+                    }
+
+                    {typeQuestion === "image" &&
+                        <ImageQuestionLoader
+                            questionImg={''}
+                            setValueQuestionImg={setImageQuestion}
+                        />
+                    }
+                </div>
+            </div>
+
+            <div className={s.answerBlock}>
+                <TextField
+                    value={valueAnswer}
+                    label="Enter your answer"
+                    margin="normal"
+                    fullWidth={true}
+                    onChange={onChangeAnswerHandler}/>
+            </div>
+        </BasicModal>
+    );
+};
